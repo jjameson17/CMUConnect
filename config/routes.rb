9048a8f4b9134a52f03688fecc5bf0fb4a416208
Jimmy Jameson <jjameson@andrew.cmu.edu>
2017-10-30 01:33:55 -0400

    signin page

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,11 @@ Rails.application.routes.draw do
   resources :mentors
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  get 'home/signin', to: 'home#signin', as: :signin
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
